526ad9942dee1126a24a5a971169041b25474bc9
Indal010 <indal.kumar010@gmail.com>
2023-03-22 08:43:45 +0530

    My first Employee Management code pushed

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            EmployeeAttendance.GetAttendance();
+        }
+    }
+}
